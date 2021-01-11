1b13c50518b778241015ae2ac4b9f91cec1dbabd
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-10 21:00:59 -0300

    Input Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputBindingComponent } from './input-binding/input-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBindingComponent
   ],
   imports: [
     BrowserModule,
